4984de4c1e3eacb9125beca86ed0ac53bc4db786
Krishandeep Singh <krishandeep17@gmail.com>
2023-09-30 09:23:52 +0530

    work with action creators

--- 09-redux-intro/src/store.js
+++ 09-redux-intro/src/store.js
@@ -6,6 +6,11 @@ const initialState = {
   loanPurpose: "",
 };
 
+// Back in the day, react developers used to place these strings into separate variables
+// const ACCOUNT_DEPOSIT = "account/deposit";
+
+const store = createStore(reducer);
+
 function reducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit": {
@@ -41,17 +46,32 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer);
+// ACTION CREATOR FUNCTIONS
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
 
-store.dispatch({ type: "account/deposit", payload: 500 });
-store.dispatch({ type: "account/withdraw", payload: 200 });
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
 console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
-});
+store.dispatch(requestLoan(1000, "Buy a car"));
 console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
+store.dispatch(payLoan());
 console.log(store.getState());
